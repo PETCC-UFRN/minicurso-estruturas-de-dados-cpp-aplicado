23b78fafc72561c10277faf42f2b9dc96079725a
thigoai <tfreire352@gmail.com>
2026-07-14 14:04:25 -0300

    melhorando estrutura das paginas e sumarios

--- assets/js/sumario_script.js
+++ assets/js/sumario_script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var toggleButton = document.getElementById("toggle-button");
     var sumario = document.getElementById("sumario");
+    var mainContent = document.querySelector("main"); 
 
     toggleButton.addEventListener("click", function() {
         sumario.classList.toggle("hidden");
+        mainContent.classList.toggle("expandido"); 
+        
         if (sumario.classList.contains("hidden")) {
             toggleButton.textContent = "Mostrar Sumário";
         } else {
