845b91910c6f951e1cebc5e5fc3054ad504ffc2a
thigoai <tfreire352@gmail.com>
2026-07-17 16:54:29 -0300

    melhorei o botão sumario

--- assets/js/sumario_script.js
+++ assets/js/sumario_script.js
@@ -2,16 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var toggleButton = document.getElementById("toggle-button");
     var sumario = document.getElementById("sumario");
     var mainContent = document.querySelector("main"); 
+    
+    // Seleciona especificamente a tag span dentro do botão
+    var buttonText = toggleButton.querySelector("span");
 
     toggleButton.addEventListener("click", function() {
         sumario.classList.toggle("hidden");
         mainContent.classList.toggle("expandido"); 
         
-        if (sumario.classList.contains("hidden")) {
-            toggleButton.textContent = "Mostrar Sumário";
-        } else {
-            toggleButton.textContent = "Esconder Sumário";
-        }
+    
     });
 
     // geração automatica de sumarios
