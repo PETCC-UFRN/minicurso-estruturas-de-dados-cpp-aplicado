d23e88a6adbdf6e962d31a38473d3f066a6df276
thigoai <tfreire352@gmail.com>
2026-07-17 17:01:16 -0300

    add h1 no sumario

--- assets/js/sumario_script.js
+++ assets/js/sumario_script.js
@@ -15,9 +15,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // geração automatica de sumarios
     var conteudoSumario = document.getElementById("conteudo-sumario");
-    
-    // Captura todos os h2 e h3 dentro da tag main
-    var headings = mainContent.querySelectorAll("h2, h3");
+   
+    var headings = mainContent.querySelectorAll("h1, h2, h3");
     
     if (headings.length > 0) {
         var html = "<ul>";
@@ -25,9 +24,11 @@ document.addEventListener("DOMContentLoaded", function() {
         headings.forEach(function(heading) {
             var id = heading.id; 
             var texto = heading.textContent;
-            var nivel = heading.tagName.toLowerCase(); // Retorna 'h2' ou 'h3'
+            var nivel = heading.tagName.toLowerCase(); // Retorna 'h1', 'h2' ou 'h3'
 
-            if (nivel === 'h2') {
+            if (nivel === 'h1') {
+                html += `<li style="margin-bottom: 12px; margin-top: 10px;"><a href="#${id}" style="color: font-size: 1.1em; text-transform: uppercase;"><strong>${texto}</strong></a></li>`;
+            } else if (nivel === 'h2') {
                 html += `<li><a href="#${id}"><strong>${texto}</strong></a></li>`;
             } else if (nivel === 'h3') {
                 html += `<li style="padding-left: 15px; font-size: 0.9em;"><a href="#${id}"> ${texto}</a></li>`;
